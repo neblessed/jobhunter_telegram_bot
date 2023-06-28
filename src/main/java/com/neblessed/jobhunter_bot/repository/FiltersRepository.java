7e1032f6753f3a1f434314c15fb4c716d98cc572
Vadim <fw.mbagg@gmail.com>
2023-06-28 17:25:09 +0300

    добавил пакет repository + интерфейсы

---
+++ src/main/java/com/neblessed/jobhunter_bot/repository/FiltersRepository.java
@@ -0,0 +1,7 @@
+package com.neblessed.jobhunter_bot.repository;
+
+import com.neblessed.jobhunter_bot.model.Filters;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FiltersRepository extends JpaRepository<Filters, Integer> {
+}
