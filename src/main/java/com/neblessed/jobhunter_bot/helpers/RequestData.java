6f87b26e99158f0b0d9ac34c7dbd28c055f61d9b
Vadim <fw.mbagg@gmail.com>
2023-07-03 19:53:37 +0300

    создал requestData класс с данными для query params

---
+++ src/main/java/com/neblessed/jobhunter_bot/helpers/RequestData.java
@@ -0,0 +1,57 @@
+package com.neblessed.jobhunter_bot.helpers;
+
+import com.neblessed.jobhunter_bot.repository.FiltersRepository;
+import com.neblessed.jobhunter_bot.repository.JobsRepository;
+import com.neblessed.jobhunter_bot.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RequestData {
+    @Autowired
+    UsersRepository usersRepository;
+
+    @Autowired
+    FiltersRepository filtersRepository;
+
+    @Autowired
+    JobsRepository jobsRepository;
+
+
+    public int getFilterId(int chatId) {
+        return usersRepository.findAll()
+                .stream()
+                .filter(x -> x.getTgUserId() == chatId)
+                .findFirst()
+                .get()
+                .getFilterId();
+    }
+
+    public int getJobId(int chatId) {
+        return filtersRepository.findById(getFilterId(chatId)).
+                get()
+                .getJobId();
+    }
+
+    public String getJobTitle(int chatId) {
+        return jobsRepository.findById(getJobId(chatId))
+                .get()
+                .getJobTitle();
+    }
+
+    public String getGrade(int chatId) {
+        return filtersRepository.findById(getFilterId(chatId))
+                .get()
+                .getGrade();
+    }
+
+    public int getSalary(int chatId) {
+        return filtersRepository.findById(getFilterId(chatId))
+                .get()
+                .getPrefferedSalary();
+    }
+
+    public String getCurrency(int chatId) {
+        return filtersRepository.findById(getFilterId(chatId))
+                .get()
+                .getCurrency();
+    }
+}
