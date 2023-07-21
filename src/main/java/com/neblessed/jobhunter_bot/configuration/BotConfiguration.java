0090c374ad9584b827a99ce5e4b6e193451d2449
Vadim <fw.mbagg@gmail.com>
2023-07-21 15:24:53 +0300

    подключил бота

--- src/main/java/com/neblessed/jobhunter_bot/configuration/BotConfiguration.java
+++ src/main/java/com/neblessed/jobhunter_bot/configuration/BotConfiguration.java
@@ -1,13 +1,8 @@
 package com.neblessed.jobhunter_bot.configuration;
 
 import com.neblessed.jobhunter_bot.bot.JobHunterBot;
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
