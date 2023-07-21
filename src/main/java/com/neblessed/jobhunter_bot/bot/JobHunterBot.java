d51b4053d326a2dbcfc82cf548045982d9cc5888
Vadim <fw.mbagg@gmail.com>
2023-07-21 20:45:55 +0300

    добавил клавиатуру

--- src/main/java/com/neblessed/jobhunter_bot/bot/JobHunterBot.java
+++ src/main/java/com/neblessed/jobhunter_bot/bot/JobHunterBot.java
@@ -1,6 +1,8 @@
 package com.neblessed.jobhunter_bot.bot;
 
+import com.neblessed.jobhunter_bot.messages.TelegramMessages;
 import com.neblessed.jobhunter_bot.service.HeadhunterApiServiceImpl;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -10,14 +12,26 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @Component
 public class JobHunterBot extends TelegramLongPollingBot {
     @Autowired
-    HeadhunterApiServiceImpl hh;
+    TelegramMessages messages;
 
     public JobHunterBot(@Value("${bot.token}") String token) {
         super(token);
     }
 
     @Override
+    @SneakyThrows
     public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage()) {
+            String message = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (message) {
+                case "/start" -> execute(messages.firstInfoMessage(chatId));
+                case "Включить приём вакансий ▶" -> execute(messages.searchModeEnabled(chatId));
+                case "Пауза ⏸" -> execute(messages.searchModeDisabled(chatId));
+            }
+        }
     }
 
     @Override
---
+++ src/main/java/com/neblessed/jobhunter_bot/keyboads/InlineKeyboards.java
@@ -0,0 +1,4 @@
+package com.neblessed.jobhunter_bot.keyboads;
+
+public class InlineKeyboards {
+}
