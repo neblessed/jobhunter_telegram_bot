0490bd1b9675d420404abd2fbad9cda63e5be1cc
Vadim <fw.mbagg@gmail.com>
2023-07-22 15:32:21 +0300

    убрал Users таблицу

--- src/main/java/com/neblessed/jobhunter_bot/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.neblessed.jobhunter_bot.repository;
-
-import com.neblessed.jobhunter_bot.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Integer> {
-}
